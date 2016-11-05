75e25778eababd376fa8f717b01ffaa2c4ef548c
Joshua Dyck <josh.dyck@inlightmedia.ca>
2016-11-05 10:02:08 -0400

    attempt at fixing cross origin issue by putting everything in one file

--- cloud/functions/hello.js
+++ cloud/functions/hello.js
@@ -5,3 +5,11 @@ Parse.Cloud.define('hello', function(req, res) {
 Parse.Cloud.define('ping', function(req, res) {
   res.success('pong');
 });
+
+Parse.Cloud.beforeSave("ChatMessage", function(request, response) {
+  if (!request.object.get("name") || !request.object.get("name") ) {
+    response.error("must have a name and message");
+  } else {
+    response.success();
+  }
+});

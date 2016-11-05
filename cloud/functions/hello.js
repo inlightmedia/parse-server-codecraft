cec31ece3de4c54185457fdf969f95d6ee871b7d
Joshua Dyck <josh.dyck@inlightmedia.ca>
2016-11-05 10:21:33 -0400

    fixed Chat Message Validation

--- cloud/functions/hello.js
+++ cloud/functions/hello.js
@@ -7,7 +7,7 @@ Parse.Cloud.define('ping', function(req, res) {
 });
 
 Parse.Cloud.beforeSave("ChatMessage", function(request, response) {
-  if (!request.object.get("name") && !request.object.get("content") ) {
+  if (!request.object.get("name") || !request.object.get("content") ) {
     response.error("must have a name and message");
   } else {
     response.success();

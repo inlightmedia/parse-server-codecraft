56ce0762d833748461055fd23f934f3ff6d42673
Joshua Dyck <josh.dyck@inlightmedia.ca>
2016-11-07 10:05:31 -0500

    changed validate to triggers from functions

--- cloud/main.js
+++ cloud/main.js
@@ -5,7 +5,7 @@ require("./functions/resetPlayerNotes");
 
 // Triggers
 // ===========
-require("./functions/validate");
+require("./triggers/validate");
 require("./triggers/testObject");
 require("./triggers/player");
 require("./triggers/chatbot");

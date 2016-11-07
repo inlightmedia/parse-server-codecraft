835e92d6370b3025b02953a392085f8da8477987
Joshua Dyck <josh.dyck@inlightmedia.ca>
2016-11-07 09:54:36 -0500

    Uncommented Cloud Code in MainJS to Invoke Error

--- cloud/main.js
+++ cloud/main.js
@@ -1,11 +1,11 @@
 // Functions
-//require("./functions/hello");
+require("./functions/hello");
 require("./functions/ping");
 require("./functions/resetPlayerNotes");
 
 // Validations
 // ===========
-// require("./functions/validate");
+require("./functions/validate");
 require("./triggers/testObject");
 require("./triggers/player");
 
